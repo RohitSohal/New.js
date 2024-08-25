f4221e35fbf0acd8baa6ef3ba3d505a96d9d043c
RohitSohal <sohalrohit0897@gmail.com>
2024-08-25 19:00:33 +0000

    Container Config done

--- 01_basics/01_Variables.js
+++ 01_basics/01_Variables.js
@@ -1,1 +1,15 @@
+const accountId = 112233
+let AccountEmail = "sohalrohit0897@gmail.com"
+let AccountPassword = "12345"
+let AccountCity = "New Delhi"
+let AccountState;
 
+// AccountId = 2 not allowed to change
+
+AccountEmail = "matharu1297@gmail.com"
+AccountPassword = "00000"
+AccountCity = "Gurugram"
+
+console.log(accountId);
+
+console.table([accountId, AccountEmail, AccountPassword, AccountCity, AccountState])
