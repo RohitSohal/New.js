4d89669e4761dc023ca6b43eafd97c205b66a538
RohitSohal <sohalrohit0897@gmail.com>
2024-08-26 08:24:14 +0000

    DataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello") we are using node js, not browser
+
+console.log(3 + 3)
+
+console.log("Rohit")
+
+// www.tc39.es for web brower help in codes ecma international Standards or MDN
+
+let name = "Rohit" // String Data Type
+let age = 27 // Number Data Type
+let isLoggedIn = true //Bolien Data Type
+
+// Data Types
+// 1. Number => 2 to power 53
+// 2. Bigint
+// 3. String => ""
+// 4. Boolean => True / False
+// 5. Null => Standalone Value
+// 6. Undefined => 
+// 7. Symbol => Unique
+
+
+
+// Object
+
+console.log(typeof null) // Object
