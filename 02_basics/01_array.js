7d19b4846109c0584728bef41cc3ee7995831234
RohitSohal <sohalrohit0897@gmail.com>
2024-08-29 18:10:29 +0000

    Array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,28 @@
+// Array
+
+const MyAray = [0, 1, 2, 3, 4, 5]
+const MyHeros = ["Shaktiman", "Chota Bheem"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(MyAray[1]);
+
+//Array methods
+
+// MyAray.push(6)
+// MyAray.push(7)
+// MyAray.pop()
+
+// MyAray.unshift(5)
+// MyAray.shift()
+
+// console.log(MyAray.includes(9));
+// console.log(MyAray.indexOf(5));
+
+const newArray = MyAray.join()
+
+// console.log(MyAray);
+// console.log(newArray);
+
+
+//slic, splice
+
